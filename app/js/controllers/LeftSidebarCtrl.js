376e75f14abdd7113f0abe72047dc7b4442043da
JulietaAtanasova <julieta.p.atanasova@gmail.com>
2015-01-10 14:14:39 +0200

    Add admin left sidebar

---
+++ app/js/controllers/LeftSidebarCtrl.js
@@ -0,0 +1,19 @@
+'use strict';
+
+app.controller('LeftSidebarCtrl',
+    function($scope, $rootScope, adsService, townsService) {
+        $scope.ads = adsService.getAll();
+        $scope.towns = townsService.getTowns();
+
+        $scope.categoryClicked = function(clickedCategoryId) {
+            $scope.selectedCategoryId = clickedCategoryId;
+            $rootScope.$broadcast("categorySelectionChanged", clickedCategoryId);
+        };
+
+        $scope.townClicked = function(clickedTownId) {
+            $scope.selectedTownId = clickedTownId;
+            $rootScope.$broadcast("townSelectionChanged", clickedTownId);
+        };
+
+    }
+);
